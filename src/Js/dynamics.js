2b47b67eeecf8de4b81404f95203074bef52c66b
Vitor Emanuel <veslima3@gmail.com>
2019-08-29 14:09:21 -0300

    Add files via upload

---
+++ src/Js/dynamics.js
@@ -0,0 +1,9 @@
+(function (){
+
+const todo = document.querySelector("#todo");
+const completed = document.querySelector("#completed"); 
+const item = document.querySelector("#item"); 
+document.querySelector("#add")
+    
+    
+})();
