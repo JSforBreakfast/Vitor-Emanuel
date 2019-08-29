72562007b952861c92d87693fa702a24261878f8
Vitor Emanuel <veslima3@gmail.com>
2019-08-29 14:57:54 -0300

    Update dynamics.js

--- src/Js/dynamics.js
+++ src/Js/dynamics.js
@@ -1,18 +1,30 @@
 (function (){
 
+//Array being used to store elements typed in by the user
+const data = {items: [];}
+
+function updateLocalStorage(){
+localStorage.setItem("todoList", JSON.stringify(data));
+}
+//Defined variables that will make functional interaction with page elements possible
 const todo = document.querySelector("#todo");
 const completed = document.querySelector("#completed"); 
 const item = document.querySelector("#item"); 
+
+//Defined event listener to respond to user clicks on the add button   
 document.querySelector("#add").addEventListener("click", addItem);
+//Defined event listener to respond to user input
 item.addEventListener("keyDown", function(e){
   if (e.code === "enter"){
      // console.log(this.value); //test on chrome dev tools
      addItem();
   }
-})    
+});    
     
 function addItem(){
-    //console.log("clicked"); //test on chrome dev tools
+    //function being used to test if there was input before the click //test on chrome dev tools
+    if(!item.value) return;
+    console.log("entered information");
 }
     
 })();
