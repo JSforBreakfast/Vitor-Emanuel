a580042a78faf97bf6a0a2e03123f3bc2be99ab5
Vitor Emanuel <veslima3@gmail.com>
2019-08-29 14:23:08 -0300

    Update dynamics.js

--- src/Js/dynamics.js
+++ src/Js/dynamics.js
@@ -3,7 +3,16 @@
 const todo = document.querySelector("#todo");
 const completed = document.querySelector("#completed"); 
 const item = document.querySelector("#item"); 
-document.querySelector("#add")
+document.querySelector("#add").addEventListener("click", addItem);
+item.addEventListener("keyDown", function(e){
+  if (e.code === "enter"){
+     // console.log(this.value); //test on chrome dev tools
+     addItem();
+  }
+})    
     
+function addItem(){
+    //console.log("clicked"); //test on chrome dev tools
+}
     
 })();
