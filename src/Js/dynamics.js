c344f477b740172574a9079a068188a693e1a866
Vitor Emanuel <veslima3@gmail.com>
2019-08-30 01:25:27 -0300

    Update dynamics.js

--- src/Js/dynamics.js
+++ src/Js/dynamics.js
@@ -4,6 +4,7 @@
 let data = localStorage.getItem("todoList") /*if there is data*/ ? JSON.parse(localStorage.getItem("todoList")):
 {items: []};//Array being used to store elements typed in by the user. If there is none, the array will remain empty.
 
+//lines used to push items into the data variable
 data.items.push("new information");
 data.items.push("new information 2");    
 console.log(data.items);
@@ -29,9 +30,23 @@ item.addEventListener("keyDown", function(e){
 });    
     
 function addItem(){
-    //function being used to test if there was input before the click //test on chrome dev tools
+    //function being used to add items written by the user //test on chrome dev tools
     if(!item.value) return;
-    console.log("entered information");
+    //console.log("entered information");
+    let currentItem = {
+        id: math.floor(Math.random()*100),
+        value: item.value,
+        completed: false
+    }
+    data.items.push(currentItem);
+    attachToDom
+    item.value = "";
+    //console.log(data.items);
+    updateLocalStorage();
 }
+   
+    
+    
+    
     
 })();
